d898999e4a51c4603b18a3873d316c9f72ae59c9
unknown <franciscolucena90@gmail.com>
2025-12-08 23:03:37 -0300

    fix: Corregir método findByCantidadParticipantesGreaterThan con consulta JPQL
    - cantidadParticipantes es un método getter, no un campo persistente
    
    - Usar consulta JPQL con SIZE() para contar participantes
    
    - Resuelve error: Could not resolve attribute 'cantidadParticipantes'

--- src/main/java/com/techlab/picadito/partido/PartidoRepository.java
+++ src/main/java/com/techlab/picadito/partido/PartidoRepository.java
@@ -5,6 +5,8 @@ import com.techlab.picadito.model.Partido;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
@@ -34,6 +36,7 @@ public interface PartidoRepository extends JpaRepository<Partido, Long>, JpaSpec
     
     List<Partido> findByFechaCreacionBetweenOrderByFechaCreacionAsc(java.time.LocalDateTime fechaInicio, java.time.LocalDateTime fechaFin);
     
-    List<Partido> findByCantidadParticipantesGreaterThan(Integer cantidad);
+    @Query("SELECT p FROM Partido p WHERE SIZE(p.participantes) > :cantidad")
+    List<Partido> findByCantidadParticipantesGreaterThan(@Param("cantidad") Integer cantidad);
 }
 
